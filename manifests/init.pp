80981fb4f3b28d06abff255f71151115a0c64237
Andreas Creten <andreas@madewithlove.be>
2014-01-15 23:43:07 +0100

    Host zip file on cloud app

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #     include changes
 class changes {
   package { 'Changes':
-    source   => 'http://bitbq.com/changes/download.php',
+    source   => 'http://c.andr.as/2P1W2x313X3z/download/changes.zip',
     provider => 'compressed_app'
   }
 }
